6e96d47258418d8b88b3e772712c0bcef40a1f3e
robbridges <robbridges@users.noreply.github.com>
2021-01-31 19:57:43 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ const hideJobRoleField= () => {
   });
 }
 
-const tShirtSelecion = () => {
+const tShirtSelection = () => {
   const tShirtDesignSelector = document.querySelector('#design');
   const tShirtColorSelector = document.querySelector('.shirt-colors');
   const tShirtOptions = tShirtColorSelector.lastElementChild.children
@@ -37,7 +37,7 @@ const tShirtSelecion = () => {
               tShirtOptions[i].setAttribute('hidden', true);
             }
 
-      } else if (e.target.value ='heart js') {
+      } else {
         colorSelector.selectedIndex = '4';
         tShirtOptions[i].removeAttribute('hidden');
         if (tShirtOptions[i].attributes['data-theme'].value !== 'heart js') {
@@ -45,12 +45,25 @@ const tShirtSelecion = () => {
         }
       }
     }
-    
-
   });
-  
-
 }
 
+const RegisterActivities = () => {
+  const activities = document.querySelector('.activities');
+  const activitiesTotal = document.querySelector('#activities-cost')
+  let total = 0;
+  activities.addEventListener('change', e => {
+    if (e.target.checked) {
+      total += parseInt(e.target.attributes['data-cost'].value);
+    } else {
+      total -= parseInt(e.target.attributes['data-cost'].value);
+    }
+    activitiesTotal.textContent = `Total: $${total}`
+  });
+}
+
+
+
 hideJobRoleField();
-tShirtSelecion();
+tShirtSelection();
+RegisterActivities();
